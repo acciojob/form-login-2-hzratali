57c957e53c25d1429d5402b58d37eb345046766b
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-02 11:56:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById('myForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        var formData = new FormData(this);
+        var formValues = {};
+        
+        formData.forEach(function(value, key){
+            formValues[key] = value;
+        });
+        
+        var alertText = '';
+        for (var key in formValues) {
+            alertText += key + ': ' + formValues[key] + ' ';
+        }
+        
+        alert(alertText.trim());
+    });
